37fbd01dfc4a2bdd607d1de0b6f1c057caac8d26
fabricio henrique alves <fabricioohh@gmail.com>
2022-12-13 23:22:03 -0300

    3° dia do Nlw

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,20 @@ import {
     Image,
     View
 } from 'react-native'
+
+import userImg from '../assets/eu.jpeg'
 import colors from "../../styles/colors";
+import fonts from "../../styles/fonts";
 
 
 export function Header(){
     return(
         <View style={styles.container}>
-
+            <View>
+                <Text style={styles.greeting}>Olá,</Text>
+                <Text style={styles.userName}>Fabricio</Text>
+            </View>
+            <Image source={userImg} style={styles.image} />
         </View>
     )
 }
@@ -22,7 +29,23 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        paddingVertical: 20,
-        backgroundColor: colors.red
+        paddingVertical: 30,
+
+    },
+    image:{
+        width: 70,
+        height: 70,
+        borderRadius: 40
+    },
+    greeting:{
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    userName:{
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight:40
     }
 })
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,18 +1,132 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
     View,
     Text,
+    FlatList,
     SafeAreaView,
     StyleSheet
 } from 'react-native';
+import { EnviromentButton } from "../components/EnviromentButton";
 
 import { Header } from "../components/Header";
+import { PlantCardPrimary } from "../components/PlantCardPrimary";
+import { Load } from "../components/Load";
+
+import api from "../services/api";
+
 import colors from "../../styles/colors";
+import fonts from "../../styles/fonts";
+
+interface EnviromentProps {
+    key: string;
+    title: string;
+}
+
+interface PlantsProps {
+    id: string,
+    name: string,
+    about: string,
+    water_tips: string,
+    photo: string,
+    environments: [string],
+    frequency: {
+        times: number,
+        repeat_every: string
+    }
+}
 
 export function PlantSelect() {
-    return(
+    const [envrioments, setEnviroments] = useState<EnviromentProps[]>()
+    const [plants, setPlants] = useState<PlantsProps[]>()
+    const [filteredPlants, setFilteredPlants] = useState<PlantsProps[]>()
+    const [enviromentSelected, setEnviromentSelected] = useState('all')
+    const [loading, setLoading] = useState(true)
+
+    function handleEnviromentSelected(environment: string) {
+        setEnviromentSelected(environment)
+
+        if (environment == 'all')
+            return setFilteredPlants(plants)
+
+        const filtered = plants?.filter(plant =>
+            plant.environments.includes(environment)
+        );
+
+        setFilteredPlants(filtered)
+    }
+
+    useEffect(() => {
+        async function fetchEnviroment() {
+            const { data } = await api.get('plants_environments?_sort=title&_order=asc');
+            setEnviroments([
+                {
+                    key: 'all',
+                    title: 'Todos',
+                },
+                ...data
+            ])
+        }
+
+        fetchEnviroment()
+
+    }, []);
+
+
+    useEffect(() => {
+        async function fetchPlants() {
+            const { data } = await api.get('plants?_sort=name&_order=asc');
+            setPlants(data)
+            setFilteredPlants(data)
+            setLoading(false)
+        }
+
+        fetchPlants()
+
+    }, [])
+
+
+    if(loading)
+    return <Load />
+    return (
         <SafeAreaView style={styles.container}>
-            <Header />
+            <View style={styles.header}>
+                <Header />
+
+                <Text style={styles.title}>
+                    Em qual ambiente
+                </Text>
+                <Text style={styles.subtitle}>
+                    Você quer colocar sua planta?
+                </Text>
+            </View>
+
+            <View>
+                <FlatList
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.enviromentList}
+                    data={envrioments}
+                    renderItem={({ item }) => (
+                        <EnviromentButton
+                            title={item.title}
+                            active={item.key === enviromentSelected}
+                            onPress={() => handleEnviromentSelected(item.key)}
+                        />
+                    )}
+                />
+            </View>
+
+            <View style={styles.plants}>
+                <FlatList
+                    data={filteredPlants}
+                    renderItem={({ item }) => (
+                        <PlantCardPrimary data={item} />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    numColumns={2}
+                />
+            </View>
+
 
         </SafeAreaView>
 
@@ -20,9 +134,37 @@ export function PlantSelect() {
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         backgroundColor: colors.background,
+    },
+    header: {
+        paddingHorizontal: 30
+    },
+    title: {
+        fontSize: 17,
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight: 20,
+        marginTop: 15
+    },
+    subtitle: {
+        fontFamily: fonts.text,
+        fontSize: 17,
+        lineHeight: 20,
+        color: colors.heading
+    },
+    enviromentList: {
+        height: 40,
+        justifyContent: 'center',
+        paddingBottom: 5,
+        marginLeft: 32,
+        marginVertical: 32
+    },
+    plants: {
+        flex: 1,
+        paddingHorizontal: 32,
+        textAlign: 'center'
+    },
 
-    }
 })
